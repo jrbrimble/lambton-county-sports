0a07276d8afea0cb6669c4701c15f5d7d7201dd5
Joe <jrbrimble@aol.com>
2026-08-21 15:06:30 +0100

    Clean up temp scripts

--- preview.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('client/src/pages/Directory.tsx', 'utf8').split('\n');
-const start = lines.findIndex(l => l.includes('<div className="min-h-screen'));
-if (start !== -1) {
-    fs.writeFileSync('preview.txt', lines.slice(start, start + 80).join('\n'));
-}
